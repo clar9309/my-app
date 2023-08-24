04beac6876ee637311c342e56adc29a587a173cf
“clar9309” <clar9309@stud.kea.dk>
2023-08-24 13:05:55 +0200

    setup tailwind colors and fonts

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,9 @@
-import Image from "next/image";
-import cat1 from "../public/images/cat1.gif";
-import cat2 from "../public/images/cat2.gif";
-import cat3 from "../public/images/cat3.gif";
+import { Main } from "next/document";
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center w-full h-screen">
-      <h1 className="text-3xl">My portfolio</h1>
+      <h1 className="font-serif text-3xl">My portfolio</h1>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -7,6 +8,17 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        white: "#E5E5E5",
+        lightgreen: "#CAD3C5",
+        darkgreen: "#43673A",
+        lightblue: "#677684",
+        darkblue: "#262B36",
+      },
+      fontFamily: {
+        serif: ["ivy presto headline", "serif"],
+        sans: ["Gotham", "sans-serif"],
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
