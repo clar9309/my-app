0c1094471a2489c6e0bf1640cbbe806cda6cd60c
“clar9309” <clar9309@stud.kea.dk>
2023-08-24 13:24:23 +0200

    test of colors and fonts

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,7 @@
-import { Main } from "next/document";
-
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center w-full h-screen">
-      <h1 className="font-serif text-3xl">My portfolio</h1>
+      <h1 className="font-serif text-3xl text-darkgreen">My portfolio</h1>
     </div>
   );
 }
